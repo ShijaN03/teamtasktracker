3e48048ee2ac47c34d22424fb13d7c74fef9e9e1
ShijaN03 <shijan.ks@yandex.ru>
2025-06-14 23:27:50 +0300

    version 1.0

---
+++ teamtasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teamtasks
+//
+//  Created by shijan on 14.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ teamtasksTests/teamtasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  teamtasksTests.swift
+//  teamtasksTests
+//
+//  Created by shijan on 14.06.2025.
+//
+
+import Testing
+@testable import teamtasks
+
+struct teamtasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
